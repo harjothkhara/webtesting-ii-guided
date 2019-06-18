e8281dbf1e7d7839eb6298acb12a6cde322a0fc8
harjothkhara <harjoth.khara@gmail.com>
2019-06-18 08:07:10 -0700

    imported libraries, tested render <App /> successfully, removed old react-testing-library and installed new one: @testing-library/react

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; 
+import { render } from "@testing-library/react"; //<<<<<<<<<<
+import 'jest-dom/extend-expect'; // <<<<<<<<<
+
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders successfully', () => {
+  render(<App />);
 });
